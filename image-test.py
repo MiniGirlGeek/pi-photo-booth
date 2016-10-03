f989e62c51ad61d1c6ef9417e87b029a1a6d17b9
Amy Mather <minigirlgeek@gmail,com>
2016-10-03 23:17:55 +0100

    fixed many bugs, green blue swap, actually produces image now etc

--- image-test.py
+++ image-test.py
@@ -3,10 +3,14 @@ from PIL import Image
 import numpy as np
 from math import floor
 
-col_adjust = np.matrix('187 49 13; 39 106 36; 30 59 86')
+width, height = 50, 75
+
+#col_adjust = np.matrix('187 49 13; 39 106 36; 30 59 86')
+#rgb were the worng way round, this fixes it
+col_adjust = np.matrix('255 0 0; 0 0 255; 0 255 0')
 col_adjust = col_adjust.I
 im = Image.open('swirl.jpg')
-im = im.resize((50, 75), PIL.Image.ANTIALIAS)
+im = im.resize((width, height), PIL.Image.ANTIALIAS)
 imdata = list(im.getdata())
 adjusted_img = []
 preview = []
@@ -14,18 +18,25 @@ preview = []
 for pixel in imdata:
     adjusted_img.append(tuple((pixel * col_adjust).tolist()[0]))
     pixels = tuple((pixel * col_adjust).tolist()[0])
-    r = floor(pixels[0] * 255)
-    g = floor(pixels[1] * 255)
-    b = floor(pixels[2] * 255)
+    r = int(pixels[0] * 255)
+    g = int(pixels[1] * 255)
+    b = int(pixels[2] * 255)
     preview.append((r, g, b))
 
 def rgb_breakdown(colour):
     values = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-    r = int(colour[0] * 10) % 10
-    g = int(colour[1] * 10) % 10
-    b = int(colour[2] * 10) % 10
+    r = int(colour[0] * 10)
+    g = int(colour[1] * 10)
+    b = int(colour[2] * 10)
+
+    if r > 10:
+        r = 10
+    if g > 10:
+        g = 10
+    if b > 10:
+        b = 10
 
     for i in range(r):
         values[0][i] = (255, 0, 0)
@@ -40,22 +51,16 @@ def rgb_breakdown(colour):
     for i in range(10-g):
         values[2][g+i] = (0, 0, 0)
 
-    print('a')
-
     new_list = []
     for l in values:
         new_list.append(l[:5])
         new_list.append(l[5:])
 
     values = new_list
-    print('c')
     for l in values:
         l.append((0, 0, 0))
 
     return values
-
-for x in rgb_breakdown((0.75, 0, 0)):
-    print(x)
 
 rgb_squares = []
 for pixel in adjusted_img:
@@ -89,16 +94,12 @@ def add_rgb_squares(width, height, breakdowns):
         for row in rows:
             for t in row:
                 output.append(t)
-        return(output)
+    return(output)
 
-im3 = Image.new(im.mode, im.size)
-im3.putdata(add_rgb_squares(50, 75, rgb_squares))
+im3 = Image.new(im.mode, (im.size[0]*6, im.size[1]*6))
+im3.putdata(add_rgb_squares(width, height, rgb_squares))
 im3.save('swrilahh.jpg')
 
-
-
-
-#print(adjusted_img)
 
 im2 = Image.new(im.mode, im.size)
 im2.putdata(preview)
